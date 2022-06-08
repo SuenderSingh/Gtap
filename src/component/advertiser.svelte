233f44a568c1befe24bf5ca41c3594c498a6488c
Prashant Malik <prashantmalik@techcompiler.com>
2022-06-08 12:24:47 +0530

    first commit

---
+++ src/component/advertiser.svelte
@@ -0,0 +1,6 @@
+<main>
+	<b class="text-xl m-2">Advertiser List</b>
+	<div class="  p-40 text-center">
+		<b>Coming Soon...</b>
+	</div>
+</main>
---
+++ src/component/campaign.svelte
@@ -0,0 +1,6 @@
+<main>
+	<b class="text-xl m-2">Campaigns </b>
+	<div class="  p-40 text-center">
+		<b>Coming Soon...</b>
+	</div>
+</main>
